cc444f36654457a2d02dd00fc5b570c5f6a59498
aashita-github <69501832+aashita-github@users.noreply.github.com>
2021-06-09 22:33:08 +0530

    Delete ImageUility directory

--- ImageUility/src/main/java/com/nagarro/interfaces/LoginInterface.java
+++
@@ -1,9 +0,0 @@
-package com.nagarro.interfaces;
-import com.nagarro.model.User;
-
-public interface LoginInterface {
-	 Boolean userAuthentication(String username, String password);
-
-	    User getUserDetails(String username);
-
-}
